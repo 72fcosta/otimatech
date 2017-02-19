4804f7299754e7670297dd75a478ad7c68a486de
Fabio Costa <72fcosta@gmail.com>
2017-02-19 09:44:26 -0300

    Commit

---
+++ _src/third/slick/setup.js
@@ -0,0 +1,18 @@
+$(function() {
+    $(".carousel-price").slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
