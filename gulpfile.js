effbb8b91e1d68b2bb357b6e4e46f3f0516135c8
Fabio Costa <72fcosta@gmail.com>
2017-05-10 05:03:26 -0300

    Commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var autoprefixer            = require("autoprefixer");
 var browserSync             = require("browser-sync").create();
 var del                     = require("del");
 var gulp                    = require("gulp");
+var cleanCSS                = require("gulp-clean-css");
 var concatJs                = require("gulp-concat");
 var concatCss               = require("gulp-concat-css");
 var git                     = require("gulp-git");
@@ -12,10 +13,9 @@ var imagemin                = require("gulp-imagemin");
 var notify                  = require("gulp-notify");
 var plumber                 = require("gulp-plumber");
 var postcss                 = require("gulp-postcss");
-var replace                 = require("gulp-replace");//jekyll
+var shell                   = require("gulp-shell");
 var sourcemaps              = require("gulp-sourcemaps");
 var stylus                  = require("gulp-stylus");
-var versionAppend           = require("gulp-version-append");
 var lost                    = require("lost");
 var rsync                   = require("rsyncwrapper");
 var runSequence             = require("run-sequence");
@@ -26,86 +26,41 @@ var rupture                 = require("rupture");
 var fs                      = require("fs");
 var varsProject             = JSON.parse(fs.readFileSync("./vars-project.json"));
 var dominio                 = varsProject.dominio;
-var regexp                  = new RegExp("https://" + dominio, "g");//jekyll
+var url                     = "https://" + varsProject.dominio;
+var diplenick               = varsProject.diplenick;
 var options                 = { }; //webfonts
 
-//--------------------------------------------------------------
+//---------------------------------------------------------FONTS
 
 gulp.task("fonts", function(callback) {
     runSequence(
         "font-del",
         "webfonts",
         callback
-        );
+        )
 });
 
 gulp.task("font-del", function() {
-    del("fonts");
+    del("fonts")
 });
 
 gulp.task("webfonts", function () {
     return gulp.src("fonts.list")
     .pipe(googleWebFonts(options))
     .pipe(gulp.dest("fonts"))
-    ;
 });
 
 //--------------------------------------------------------------
 
-gulp.task("replace-in", function() {
-    return gulp.src("_config.yml")
-        .pipe(replace(/http:\/\/localhost:3000/g, "https://" + dominio))
-        .pipe(gulp.dest(""));
-});
+gulp.task("replace-in", shell.task(
+    "python3.5 -c \'from gulp import jk_replace_lines_in; jk_replace_lines_in(\"" + diplenick + "\", \"" + url + "\")\'"
+));
 
-gulp.task("replace-out", function() {
-    return gulp.src("_config.yml")
-        .pipe(replace(regexp, "http://localhost:3000"))
-        .pipe(gulp.dest(""));
-});
+gulp.task("replace-out", shell.task(
+    "python3.5 -c \'from gulp import jk_replace_lines_out; jk_replace_lines_out(\"" + diplenick + "\", \"" + url + "\")\'"
+));
 
-//--------------------------------------------------------------
-
-gulp.task("third-css", function () {
-    return gulp.src("_src/third/**/*.css")
-        .pipe(concatCss("third.css"))
-        .pipe(gulp.dest("_site/assets/css/"))
-        .pipe(gulp.dest("assets/css/"));
-});
-
-gulp.task("third-js", function () {
-    return gulp.src("_src/third/**/*.js")
-        .pipe(concatJs("third.js"))
-        .pipe(gulp.dest("assets/js/"));
-});
-
-//--------------------------------------------------------------
-
-gulp.task("jekyll-build", function (done) {
-    browserSync.notify("Building Jekyll");
-    return cp.spawn("jekyll", ["build"], {stdio: "inherit"})
-        .on("close", done);
-});
-
-gulp.task("jekyll-rebuild", ["jekyll-build"], function () {
-    browserSync.reload();
-});
-
-gulp.task("browserSync-reload", function () {
-    browserSync.reload();
-});
-
-//--------------------------------------------------------------
-
-gulp.task("browser-sync", ["jekyll-build"], function() {
-    browserSync.init({
-        server: {
-            baseDir: "_site"
-        }
-    });
-});
-
-//--------------------------------------------------------------
+//-----------------------------------------------------------CSS
 
 gulp.task("stylus", function() {
     return gulp.src("_src/styl/style.styl")
@@ -120,29 +75,83 @@ gulp.task("stylus", function() {
     .pipe(postcss([
         lost(),
         autoprefixer()
-    ]))
-    .pipe(sourcemaps.write("."))
+        ]))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest("_site/assets/css/"))
+    .pipe(browserSync.stream())
     .pipe(gulp.dest("assets/css/"))
-    .pipe(browserSync.stream());
 });
 
-//--------------------------------------------------------------
-
-gulp.task("timestamp", function () {
-    return gulp.src("_site/*.html")
-        .pipe(versionAppend(["html", "js", "css"], {appendType: "timestamp"}))
-        .pipe(gulp.dest("./_site/"));
+gulp.task("third-css", function() {
+    return gulp.src("_src/third/**/*.css")
+    .pipe(concatCss("third.css"))
+    .pipe(gulp.dest("assets/css/"))
 });
 
-//--------------------------------------------------------------
+gulp.task("min-css", function() {
+    return gulp.src("_site/assets/css/*.css")
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("_site/assets/css/"))
+});
+
+//------------------------------------------------------------JS
 
 gulp.task("js", function() {
     rsync({
         src: "_src/js/script.js",
         dest: "assets/js/script.js",
         recursive: true,
-        args: [ "--verbose" ],
+        args: [ "-v" ],
+        delete: true,
+        compareMode: "checksum",
+        onStdout: function (data) {
+            console.log(data.toString());
+        }
+    }, function() {
+        console.log("End")
+    });
+});
+
+gulp.task("third-js", function() {
+    return gulp.src("_src/third/**/*.js")
+    .pipe(concatJs("third.js"))
+    .pipe(gulp.dest("assets/js/"))
+});
+
+//--------------------------------------------------------------
+
+gulp.task("jekyll-build", function(done) {
+    browserSync.notify("Building Jekyll");
+    return cp.spawn("jekyll", ["build"], {stdio: "inherit"})
+        .on("close", done)
+});
+
+gulp.task("jekyll-rebuild", ["jekyll-build"], function() {
+    browserSync.reload()
+});
+
+//--------------------------------------------------------------
+
+gulp.task("browser-sync", ["jekyll-build"], function() {
+    browserSync.init({
+        server: {
+            baseDir: "_site"
+        }
+    });
+});
+
+gulp.task("browserSync-reload", function() {
+    browserSync.reload()
+});
+
+//--------------------------------------------------------------
+
+gulp.task("sync-favicon", function() {
+    rsync({
+        src: ["_src/favicon/"],
+        dest: "assets/favicon/",
+        recursive: true,
+        args: ["-v"],
         delete: true,
         compareMode: "checksum",
         onStdout: function (data) {
@@ -153,47 +162,26 @@ gulp.task("js", function() {
     });
 });
 
-//--------------------------------------------------------------
-
-gulp.task("image-watch", ["image"], function (done) {
-    browserSync.reload();
-    done();
+gulp.task("sync-img", function() {
+    rsync({
+        src: ["_src/img"],
+        dest: "assets/",
+        recursive: true,
+        args: ["-v"],
+        delete: true,
+        compareMode: "checksum",
+        onStdout: function (data) {
+            console.log(data.toString());
+        }
+    }, function() {
+        console.log("End");
+    });
 });
 
-gulp.task("image", function(callback) {
-    runSequence(
-        "image-del",
-        "imagemin",
-        callback
-        );
-});
-
-gulp.task("image-del", function() {
-    return del(["assets/img/"]);
-});
-
-gulp.task("imagemin", function() {
+gulp.task("min-image", function() {
     return gulp.src("_src/img/*.*")
     .pipe(imagemin())
-    .pipe(gulp.dest("assets/img/"));
-});
-
-//--------------------------------------------------------------
-
-gulp.task("favicon", function() {
-    rsync({
-        src: "_src/favicon/",
-        dest: "assets/favicon",
-        recursive: true,
-        args: [ "--verbose" ],
-        delete: true,
-        compareMode: "checksum",
-        onStdout: function (data) {
-            console.log(data.toString());
-        }
-    }, function() {
-        console.log("End");
-    });
+    .pipe(gulp.dest("assets/img/"))
 });
 
 //--------------------------------------------------------------
@@ -223,7 +211,7 @@ gulp.task("linode", function() {
         ssh: true,
         recursive: true,
         exclude: [".htaccess", "gulpfile.js", "config", "description", "HEAD", "hooks", "tarefas-do-projeto.txt"],
-        args: [ "--verbose" ],
+        args: ["-v"],
         delete: true,
         compareMode: "checksum",
         onStdout: function (data) {
@@ -237,14 +225,14 @@ gulp.task("linode", function() {
 //--------------------------------------------------------------
 
 gulp.task("watch", function () {
-    gulp.watch(["fonts.list"], ["fonts"]).on("change", browserSync.reload);
-    gulp.watch("_src/third/**/*.css", ["third-css", "jekyll-rebuild"]);
-    gulp.watch("_src/third/**/*.js", ["third-js", "jekyll-rebuild"]);
-    gulp.watch("_src/img/**/*", ["image-watch", "jekyll-rebuild"]);
-    gulp.watch("_posts/**/*", ["jekyll-rebuild"]);
-    gulp.watch("_src/js/*.js", ["js", "jekyll-rebuild"]);
-    gulp.watch("_src/styl/**/*.styl", ["stylus"]);
-    gulp.watch(["*.html", "_includes/**/*.html", "_layouts/*.html", "_config.yml"], ["jekyll-rebuild"]);
+    gulp.watch(["*.html", "_includes/**/*.html", "_layouts/*.html", "_config.yml", "_posts/**/*"], ["jekyll-rebuild"]);
+    gulp.watch(["_src/styl/**/*.styl"], ["stylus"]);
+    gulp.watch(["_src/third/**/*.css"], ["third-css", "jekyll-rebuild"]);
+    gulp.watch(["_src/js/*.js"], ["js", "jekyll-rebuild"]);
+    gulp.watch(["_src/third/**/*.js"], ["third-js", "jekyll-rebuild"]);
+    gulp.watch(["_src/favicon/*"], ["sync-favicon", "jekyll-rebuild"]);
+    gulp.watch(["_src/img/**/*"], ["sync-img", "jekyll-rebuild"]);
+    gulp.watch(["fonts.list"], ["fonts", "jekyll-rebuild"]);
 });
 
 //--------------------------------------------------------------
@@ -252,13 +240,13 @@ gulp.task("watch", function () {
 //F6
 gulp.task("reconstruir", function(callback) {
     runSequence(
-        "third-css",
-        "third-js",
-        "favicon",
-        "image",
-        "js",
-        "fonts",
         "stylus",
+        "third-css",
+        "js",
+        "third-js",
+        "sync-favicon",
+        "sync-img",
+        "fonts",
         "jekyll-build",
         "browserSync-reload",
         callback
@@ -273,7 +261,8 @@ gulp.task("deploy", function(callback) {
         "git-commit",
         "git-push",
         "jekyll-build",
-        "timestamp",
+        "min-css",
+        "min-image",
         "linode",
         "replace-out",
         callback
@@ -283,13 +272,6 @@ gulp.task("deploy", function(callback) {
 //F9
 gulp.task("letscode", function(callback) {
     runSequence(
-        "third-css",
-        "third-js",
-        "favicon",
-        "image",
-        "js",
-        "fonts",
-        "stylus",
         "browser-sync",
         "watch",
         callback
